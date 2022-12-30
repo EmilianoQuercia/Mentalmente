bad2f08bfc5c5726fcc603ed05fc02cf3763766e
EmilianoQuercia <emilianoquercia1@gmail.com>
2022-12-30 20:46:13 -0300

    comente Pantalla preguntas para hacer unna prueba

--- Mentalmente/src/routes/AppRoutes.jsx
+++ Mentalmente/src/routes/AppRoutes.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
+
 import { Routes, Route } from "react-router-dom";
 
 import Ajustes from "../components/Ajustes/Ajustes";
 import PantallaFinal from "../components/pantallaFinal/PantallaFinal";
 import PantallaInicio from "../components/pantallaInicio/PantallaInicio";
 import PantallaMenu from "../components/pantallaMenu/PantallaMenu";
-import PantallaPreguntas from "../components/pantallaPreguntas/PantallaPreguntas";
+// import PantallaPreguntas from "../components/pantallaPreguntas/PantallaPreguntas";
 import SoloCorrectas from "../components/pantallaPreguntas/SoloCorrectas";
 
 const AppRoutes = () =>{
-    // const [puntos, setPuntos] = useState(0)
+   
     return(
         <>
          <Routes>
             <Route path='/' element={<PantallaInicio />}/>
             <Route path='/menu' element={<PantallaMenu/>}/> 
-            <Route path='/Contra Reloj' element={<PantallaPreguntas />}/>
+            {/* <Route path='/Contra Reloj' element={<PantallaPreguntas />}/> */}
             <Route path='/Solo Correctas' element={<SoloCorrectas />}/>
             <Route path='/ajustes' element={<Ajustes/>}/>
             <Route path='/final' element= {<PantallaFinal />}/>
